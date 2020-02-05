ad287f2e6dcf8a558809d8d160e8a89d7ac8ff1f
Pleuvens Fervil <pleuvens.fervil@gmail.com>
2020-02-05 21:37:58 +0100

    [CPU] - started class skeleton

---
+++ src/chip8/cpu.js
@@ -0,0 +1,82 @@
+class  CPU {
+    constructor(attributes) {
+        this.attributes = attributes;
+    }
+
+    execute(opcode) {
+        switch (opcode) {
+            case '0NNN':
+                break;
+            case '00E0':
+                break;
+            case '00EE':
+                break;
+            case '1NNN':
+                break;
+            case '2NNN':
+                break;
+            case '3XNN':
+                break;
+            case '4XNN':
+                break;
+            case '5XY0':
+                break;
+            case '6XNN':
+                break;
+            case '7XNN':
+                break;
+            case '8XY0':
+                break;
+            case '8XY1':
+                break;
+            case '8XY2':
+                break;
+            case '8XY3':
+                break;
+            case '8XY4':
+                break;
+            case '8XY5':
+                break;
+            case '8XY6':
+                break;
+            case '8XY7':
+                break;
+            case '8XYE':
+                break;
+            case '9XY0':
+                break;
+            case 'ANNN':
+                break;
+            case 'BNNN':
+                break;
+            case 'CXNN':
+                break;
+            case 'DXYN':
+                break;
+            case 'EX9E':
+                break;
+            case 'EXA1':
+                break;
+            case 'FX07':
+                break;
+            case 'FX0A':
+                break;
+            case 'FX15':
+                break;
+            case 'FX18':
+                break;
+            case 'FX1E':
+                break;
+            case 'FX29':
+                break;
+            case 'FX33':
+                break;
+            case 'FX55':
+                break;
+            case 'FX65':
+                break;
+            default:
+                break;
+        }
+    }
+}
